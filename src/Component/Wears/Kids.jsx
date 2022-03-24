5ba243e417b8a463ebc72960dff4621b21b0fcbd
Sujay Singh <sujaysingh29@gmail.com>
2022-03-24 16:00:44 +0530

    fourth commit

--- src/Component/Wears/Kids.jsx
+++ src/Component/Wears/Kids.jsx
@@ -1,19 +1,155 @@
-import React from 'react'
-import { Dropdown } from 'react-bootstrap'
+import React from "react";
+import { Dropdown, Row, Col } from "react-bootstrap";
+import DropdownMenu from "react-bootstrap/esm/DropdownMenu";
 
 const Kids = () => {
   return (
-    <div><Dropdown>
+    <Dropdown>
       <Dropdown.Toggle variant="none">
         Kids
+        <DropdownMenu className="p-2" style={{ width: "1400px",marginRight:'70px' }}>
+          <Row>
+            <Col>
+              <Dropdown.Item style={{ color: "red" }} href="#/action-3">
+                Boys Clothing
+              </Dropdown.Item>
+              <Dropdown.Item href="#/action-3">T-Shirts</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Shirts</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Shorts</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Jeans</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Trousers</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Clothing Sets</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Ethnic Wear</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Track Pants & Pyjamas</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Jacket, Sweater & Sweatshirts</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">
+              Party Wear
+              </Dropdown.Item>
+              <Dropdown.Item href="#/action-3">
+              Innerwear & Thermals
+              </Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Nightwear & Loungewear</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Value Packs</Dropdown.Item>
+            </Col>
+            <Col>
+              <Dropdown.Item style={{ color: "red" }} href="#/action-3">
+                Girls Clothing
+              </Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Dresses</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Tops</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Tshirts</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Clothing Sets</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Lehenga choli</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">
+              Kurta Sets
+              </Dropdown.Item>
+              <Dropdown.Item href="#/action-3">
+              Party wear
+              </Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Dungarees & Jumpsuits
+</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Skirts & shorts</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Tights & Leggings</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">
+              Jeans, Trousers & Capris
+              </Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Jacket, Sweater & Sweatshirts</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Innerwear & Thermals</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">
+              Nightwear & Loungewear
+              </Dropdown.Item>
+
+              <Dropdown.Item href="#/action-2">Value Packs</Dropdown.Item>
+            </Col>
+
+            <Col>
+            <Dropdown.Item style={{ color: "red" }} href="#/action-3">
+                Footwear
+              </Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Sports Shoes</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Formal Shoes</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Sneakers</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">
+                Sandals & Floaters
+              </Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Flip Flops</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Socks</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Blazers & Coats</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Suits</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Rain Jackets</Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Item style={{ color: "red" }} href="#/action-3">
+                Personal Care & Grooming
+              </Dropdown.Item>
+              <Dropdown.Item style={{ color: "red" }} href="#/action-3">
+                Sunglasses & Frames
+              </Dropdown.Item>
+              <Dropdown.Item style={{ color: "red" }} href="#/action-3">
+                Watches
+              </Dropdown.Item>
+            </Col>
+            <Col>
+              <Dropdown.Item style={{ color: "red" }} href="#/action-2">
+                Sports & Active Wear
+              </Dropdown.Item>
+
+              <Dropdown.Item href="#/action-2">Sports Shoes</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Sports Sandals</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Active T-Shirts</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">
+                Track Pants & Shorts
+              </Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Tracksuits</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">
+                Sports Accessories
+              </Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Swimwear</Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Item style={{ color: "red" }} href="#/action-3">
+                Gadgets
+              </Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Smart Wearables</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Fitness Gadgets</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Headphones</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Speakers</Dropdown.Item>
+            </Col>
+            <Col>
+              <Dropdown.Item style={{ color: "red" }} href="#/action-2">
+                Fashion Accessories
+              </Dropdown.Item>
+
+              <Dropdown.Item href="#/action-2">Wallets</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Belts</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">
+                Perfumes & Body Mists
+              </Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Trimmers</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Deodorants</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">
+                Ties, Cufflinks & Pocket Squares
+              </Dropdown.Item>
+              <Dropdown.Item href="#/action-3">
+                Accessory Gift Sets
+              </Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Caps & Hats</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">
+                Mufflers, Scarves & Gloves
+              </Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Phone Cases</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Rings & Wristwear</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Helmets</Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Item style={{ color: "red" }} href="#/action-3">
+                Bags & Backpacks
+              </Dropdown.Item>
+              <Dropdown.Item style={{ color: "red" }} href="#/action-3">
+                Luggages & Trolleys
+              </Dropdown.Item>
+            </Col>
+          </Row>
+        </DropdownMenu>
       </Dropdown.Toggle>
-      <Dropdown.Menu>
-    <Dropdown.Item href="#/action-1">Shirts</Dropdown.Item>
-    <Dropdown.Item href="#/action-2">Casual Wear</Dropdown.Item>
-    <Dropdown.Item href="#/action-3">Party Wear</Dropdown.Item>
-  </Dropdown.Menu>
     </Dropdown>
-    </div>
   )
 }
 
--- src/Component/Wears/Men.jsx
+++ src/Component/Wears/Men.jsx
@@ -1,52 +1,155 @@
 import React from "react";
-import { Container, Dropdown, Row, Col } from "react-bootstrap";
-import DropdownItem from "react-bootstrap/esm/DropdownItem";
+import { Dropdown, Row, Col } from "react-bootstrap";
 import DropdownMenu from "react-bootstrap/esm/DropdownMenu";
+import "./men.css"
 
 const Men = () => {
   return (
-    
-      <Dropdown >
-        <Dropdown.Toggle variant="none">
-          Men
-              <DropdownMenu className='p-sm-2' style={{width:'800px'}}>
-          <Row >
-            <Col xs>
-                <Dropdown.Item href="#/action-3">Party Wear</Dropdown.Item>
-            </Col>
-            <Col xs>
-              <Dropdown.Item href="#/action-2">Casual Wear</Dropdown.Item>
-            </Col>
-            <Col xs>
-              <Dropdown.Item href="#/action-2">Casual Wear</Dropdown.Item>
-
-            </Col>
-            <Col xs>
-              <Dropdown.Item href="#/action-2">Casual Wear</Dropdown.Item>
-
-            </Col>
-          </Row>
+    <Dropdown>
+      <Dropdown.Toggle variant="none">
+        Men
+        <DropdownMenu className="p-4" style={{ width: "1500px",marginRight:'70px' }}>
           <Row>
-            <Col xs>
-                <Dropdown.Item href="#/action-3">Party Wear</Dropdown.Item>
+            <Col>
+              <Dropdown.Item style={{ color: "red" }} href="#/action-3">
+                Top Wear
+              </Dropdown.Item>
+              <Dropdown.Item href="#/action-3">T-shirts</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Casual shirts</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Formal shirts</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Sweat shirts</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Sweaters</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Jackets</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Blazers & Coats</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Suits</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Rain Jackets</Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Item style={{ color: "red" }} href="#/action-3">
+                Indian & Festive Wear
+              </Dropdown.Item>
+              <Dropdown.Item href="#/action-3">
+                Kurtas & Kurta Sets
+              </Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Sherwanis</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Nehru Jackets</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Dhotis</Dropdown.Item>
             </Col>
             <Col>
-              <Dropdown.Item href="#/action-2">Casual Wear</Dropdown.Item>
+              <Dropdown.Item style={{ color: "red" }} href="#/action-3">
+                Bottomwear
+              </Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Jeans</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Casual Trousers</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Formal Trousers</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Sweat shirts</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Shorts</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">
+                Track Pants & Joggers
+              </Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Item href="#/action-3" style={{ color: "red" }}>
+                Innerwear & Sleepwear
+              </Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Briefs & Trunks</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Boxers</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Vests</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">
+                Sleepwear & Loungewear
+              </Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Thermals</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Plus Size</Dropdown.Item>
+            </Col>
 
+            <Col>
+              <Dropdown.Item style={{ color: "red" }} href="#/action-2">
+                Footwear
+              </Dropdown.Item>
+
+              <Dropdown.Item href="#/action-2">Casual Shoes</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Sports Shoes</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Formal Shoes</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Sneakers</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">
+                Sandals & Floaters
+              </Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Flip Flops</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Socks</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Blazers & Coats</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Suits</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Rain Jackets</Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Item style={{ color: "red" }} href="#/action-3">
+                Personal Care & Grooming
+              </Dropdown.Item>
+              <Dropdown.Item style={{ color: "red" }} href="#/action-3">
+                Sunglasses & Frames
+              </Dropdown.Item>
+              <Dropdown.Item style={{ color: "red" }} href="#/action-3">
+                Watches
+              </Dropdown.Item>
             </Col>
             <Col>
-              <Dropdown.Item href="#/action-2">Casual Wear</Dropdown.Item>
+              <Dropdown.Item style={{ color: "red" }} href="#/action-2">
+                Sports & Active Wear
+              </Dropdown.Item>
 
+              <Dropdown.Item href="#/action-2">Sports Shoes</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Sports Sandals</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Active T-Shirts</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">
+                Track Pants & Shorts
+              </Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Tracksuits</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">
+                Sports Accessories
+              </Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Swimwear</Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Item style={{ color: "red" }} href="#/action-3">
+                Gadgets
+              </Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Smart Wearables</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Fitness Gadgets</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Headphones</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Speakers</Dropdown.Item>
             </Col>
             <Col>
-              <Dropdown.Item href="#/action-2">Casual Wear</Dropdown.Item>
+              <Dropdown.Item style={{ color: "red" }} href="#/action-2">
+                Fashion Accessories
+              </Dropdown.Item>
 
+              <Dropdown.Item href="#/action-2">Wallets</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Belts</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">
+                Perfumes & Body Mists
+              </Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Trimmers</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Deodorants</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">
+                Ties, Cufflinks & Pocket Squares
+              </Dropdown.Item>
+              <Dropdown.Item href="#/action-3">
+                Accessory Gift Sets
+              </Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Caps & Hats</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">
+                Mufflers, Scarves & Gloves
+              </Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Phone Cases</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Rings & Wristwear</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Helmets</Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Item style={{ color: "red" }} href="#/action-3">
+                Bags & Backpacks
+              </Dropdown.Item>
+              <Dropdown.Item style={{ color: "red" }} href="#/action-3">
+                Luggages & Trolleys
+              </Dropdown.Item>
             </Col>
           </Row>
-         
-              </DropdownMenu>
-        </Dropdown.Toggle>
-      </Dropdown>
+        </DropdownMenu>
+      </Dropdown.Toggle>
+    </Dropdown>
   );
 };
 
